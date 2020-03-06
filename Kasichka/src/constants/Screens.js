b427b1ec2e9244f89e4df4a1c5e558106c52e4ed
Paco Minev <p.minev.96@abv.bg>
2020-03-07 00:39:13 +0200

    Changed Bottom navigator to class and created new class for budget overview

---
+++ Kasichka/src/constants/Screens.js
@@ -0,0 +1,11 @@
+export const ScreenNames = {
+    Overview     : 'Overview',
+    Transactions : 'Transactions',
+    Settings     : 'Settings',
+}
+
+export const ScreenTitles = {
+    Overview     : 'Overview',
+    Transactions : 'Transactions',
+    Settings     : 'Settings',
+}
--- Kasichka/src/navigation/BottomTabNavigator.js
+++ Kasichka/src/navigation/BottomTabNavigator.js
@@ -1,58 +1,70 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { ScreenNames, ScreenTitles } from '../constants/Screens'
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import CustomTab from '../screens/CustomTab';
+import BudgetOverview from '../screens/BudgetOverview' 
 
 const BottomTab = createBottomTabNavigator();
-const INITIAL_ROUTE_NAME = 'Home';
+const INITIAL_ROUTE_NAME = ScreenNames.Overview;
 
-export default function BottomTabNavigator({ navigation, route }) {
+export default class BottomTabNavigator extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.props = props;
+
+    props.navigation.setOptions({ headerTitle: getHeaderTitle(props.route) });
+  }
+
   // Set the header title on the parent stack navigator depending on the
   // currently active tab. Learn more in the documentation:
   // https://reactnavigation.org/docs/en/screen-options-resolution.html
-  navigation.setOptions({ headerTitle: getHeaderTitle(route) });
+  render() {
+    this.props.navigation.setOptions({ headerTitle: getHeaderTitle(this.props.route) });
 
-  return (
-    <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
-      <BottomTab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          title: 'Get Started',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
-        }}
-      />
-      <BottomTab.Screen
-        name="Links"
-        component={LinksScreen}
-        options={{
-          title: 'Resources',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
-        }}
-      />
-      <BottomTab.Screen
-        name="CustomTab"
-        component={CustomTab}
-        options={{
-          title: 'Custom Buttom Tab',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="ios-list" />,
-        }}
-      />
-    </BottomTab.Navigator>
-  );
+    return (
+      <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
+        <BottomTab.Screen
+          name={ScreenNames.Overview}
+          component={BudgetOverview}
+          options={{
+            title: ScreenTitles.Overview,
+            tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
+          }}
+        />
+        <BottomTab.Screen
+          name={ScreenNames.Transactions}
+          component={LinksScreen}
+          options={{
+            title: ScreenTitles.Transactions,
+            tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
+          }}
+        />
+        <BottomTab.Screen
+          name={ScreenNames.Settings}
+          component={CustomTab}
+          options={{
+            title: ScreenTitles.Settings,
+            tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="ios-list" />,
+          }}
+        />
+      </BottomTab.Navigator>
+    );
+  }  
 }
 
 function getHeaderTitle(route) {
   const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
 
   switch (routeName) {
-    case 'Home':
-      return 'How to get started';
-    case 'Links':
-      return 'Links to learn more';
-    case 'CustomTab':
-      return 'My first custom tab';
+    case ScreenNames.Overview:
+      return ScreenTitles.Overview;
+    case ScreenNames.Transactions:
+      return ScreenTitles.Transactions;
+    case ScreenNames.Settings:
+      return ScreenTitles.Settings;
   }
 }
